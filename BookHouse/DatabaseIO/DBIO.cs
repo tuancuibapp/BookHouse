00c93e4ee7b925f7c7eb070e980d7a4e8d885b08
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2021-12-10 15:37:22 +0700

    .

---
+++ BookHouse/DatabaseIO/DBIO.cs
@@ -0,0 +1,29 @@
+﻿using DatabaseProvider.MyDB;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseIO
+{
+    public class DBIO
+    {
+        MyDB mydb = new MyDB();
+        public Img GetObject_User()
+        {
+            /*string SQL = "SELECT * FROM TVLT_USers WHERE Uid = '"+uid+"' AND Pwd='" + pass + "'";
+            return mydb.Database.SqlQuery<TVLT_Users>(SQL).FirstOrDefault();*/
+            /*return mydb.Database.SqlQuery<Book>(
+                "SELECT * FROM Img",
+                new SqlParameter("@U", uid),
+                new SqlParameter("@P", pass)
+                ).FirstOrDefault();*/
+            return mydb.Database.SqlQuery<Img>(
+                "SELECT * FROM Img"
+                ).FirstOrDefault();
+        }
+        
+    }
+}
