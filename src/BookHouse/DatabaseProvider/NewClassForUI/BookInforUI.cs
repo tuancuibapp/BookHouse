7ef45acf3fcd846fa3c48007030e80e120f3bbff
vntuan13579 <64194723+vntuan13579@users.noreply.github.com>
2021-12-10 15:52:15 +0700

    haiza

--- src/BookHouse/DatabaseProvider/NewClassForUI/BookInforUI.cs
+++ src/BookHouse/DatabaseProvider/NewClassForUI/BookInforUI.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using DatabaseProvider.MyDB;
 namespace DatabaseProvider.NewClassForUI
 {
     public class BookInforUI
     {
+        public Book book { get; set; }
+        public float rating { get; set; }
+        public List<string> comments { get; set; }
+        public List<string> images { get; set; }
     }
 }
