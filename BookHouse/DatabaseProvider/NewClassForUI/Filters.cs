d246f2d00d56b034312ea866169a916ca6b08294
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2021-12-10 14:18:56 +0700

    .

---
+++ BookHouse/DatabaseProvider/NewClassForUI/Filters.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseProvider.NewClassForUI
+{
+    public class Filters
+    {
+        public List<string> categories { get; set; }
+        public List<bool> values { get; set; }
+        public List<int> priceRange { get; set; }
+        public float rating { get; set; }
+        public bool sortAZ { get; set; }
+        public bool sortInc { get; set; }
+    }
+}
---
+++ BookHouse/DatabaseProvider/NewClassForUI/SearchingUI.cs
@@ -0,0 +1,15 @@
+﻿using DatabaseProvider.MyDB;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseProvider.NewClassForUI
+{
+    public class SearchingUI
+    {
+        public List<Book> books { get; set; }
+        public Filters filters { get; set; }
+    }
+}
