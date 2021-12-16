d6b395e11d285493c2d1631bcbe34e686a50f2d0
DuongVanKhoa0811 <dvkhoa19@apcs.fitus.edu.vn>
2021-12-16 15:00:41 +0700

    .

--- src/BookHouse/BookHouse/Controllers/UserController.cs
+++ src/BookHouse/BookHouse/Controllers/UserController.cs
@@ -64,7 +64,61 @@ namespace BookHouse.Controllers
         }
         public ActionResult SearchPage()
         {
+<<<<<<< Updated upstream
             return View();
+=======
+            //create filters
+            Filters filters = new Filters
+            {
+                categories = new List<string> {
+                    "Tiểu thuyết", "Truyện ngắn", "Thơ", "Trinh thám", "Truyện tranh", "Lịch sử", "Triết học",
+                    "Kinh tế", "Tâm lý học", "Tham khảo", "Viễn tưởng" },
+
+                values = new List<bool> {
+                    false, false, false, false, false,
+                    false, false, false, false, false, false },
+
+                rating = 0,
+                sortInc = false,
+                sortDec = false,
+                priceRange = new List<int>{ -1, -1 },
+                query = Query
+            };
+
+            //here is for query the books from database using filters
+            BookInforUI yay = new BookInforUI
+            {
+                book = new Book
+                {
+                    BookName = "My life is not a joke, jokes have meaning.",
+                    BookID = "1",
+                    Price = 100000
+                },
+
+                rating = 4,
+                images = new List<string> { "https://toplist.vn/images/800px/dac-nhan-tam-116541.jpg" }
+            };
+
+            List<BookInforUI> queryResult = new List<BookInforUI>
+            {yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay,
+            yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay,
+            yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay, yay};
+
+            //get the info for the view
+            int pageSize = 20;
+            ViewData["Filters"] = filters;
+            ViewData["CurrentPage"] = page;
+
+            int NumPage = queryResult.Count / pageSize;
+            if (queryResult.Count % pageSize != 0)
+                NumPage++;
+            ViewData["NumPage"] = NumPage;
+            ViewData["Query"] = Query;
+
+            List<BookInforUI> searchData = queryResult.GetRange(pageSize * page, Math.Min(pageSize, queryResult.Count - pageSize * page));
+            List<BookInforUI> u = new List<BookInforUI>;
+            return View(u);
+>>>>>>> Stashed changes
         }
         public ActionResult FAQ()
         {
